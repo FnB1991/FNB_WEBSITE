92ca75a2cdcd46c18ad1bd4232c839641f23ab7e
FnB1991 <129508718+FnB1991@users.noreply.github.com>
2023-04-23 10:01:51 +0700

    Updates
    - Formatação do código;
    - Atribuído novas requests;
    - Adicionado campo $assunto.

--- send_email.php
+++ send_email.php
@@ -1,21 +1,23 @@
 <?php
 
-$destinatario = "seuemail@seudominio";
+$destinatario = "felipenauar@yahoo.com.br";
 
 $assunto = "[FNB DEVELOPERS] - NOVO CONTATO RECEBIDO";
-$nome = $_REQUEST['nome'];
-$fone = $_REQUEST['fone'];
+$nome = $_REQUEST['name'];
 $email = $_REQUEST['email'];
+$fone = $_REQUEST['fone'];
 
-// FORMATO DO EMAIL NA VARIÁVEL $body
+// == FORMATO DO E-MAIL NA VARIAVÉL $BODY == //
 
-$body = $body . "Assunto: " . $assunto . "\n";
+$body = $body . "Assunto: " . $assunto . "\n\n";
 $body = $body . "Nome: " . $nome . "\n";
-$body = $body . "Fone: " . $fone . "\n";
 $body = $body . "E-mail: " . $email . "\n";
+$body = $body . "Fone: " . $fone . "\n";
 
-// DISPARA E-MAIL
+// == DISPARA E-MAIL COM BASE NAS CONFIGS $ == /
 mail($destinatario, $assunto, $body, "From: $email\r\n");
 
-// REDIRECIONANDO
+// == APÓS ENVIAR, REDIRECIONA PARA PÁGINA OBRIGADO == /
 header("thanks.html");
+
+?>
